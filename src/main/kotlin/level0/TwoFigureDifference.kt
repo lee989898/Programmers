3253717e59499582fe4ed7e2261cb2c77405c7b7
lee989898 <wnsrud1352@gmail.com>
2023-01-11 23:45:44 +0900

    [feat](level0) 두 수의 차 구현

---
+++ src/main/kotlin/level0/TwoFigureDifference.kt
@@ -0,0 +1,10 @@
+package level0
+
+object TwoFigureDifference {
+    fun solution(num1: Int, num2: Int) = num1 - num2
+}
+
+fun main() {
+    println(TwoFigureDifference.solution(2, 3))
+    println(TwoFigureDifference.solution(100, 2))
+}
